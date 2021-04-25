0b74510892faeafdd956a80a9b0ef8a43c1d80f2
Stefan Turzer <turzer.stefan@gmail.com>
2021-04-25 22:27:06 +0200

    redo serveless fun

--- api/test.js
+++ api/test.js
@@ -1,14 +1,14 @@
 const Pusher = require("pusher");
 
-module.exports = (req, res) => {
-  const pusher = new Pusher({
-    appId: process.env.pusher_app_id,
-    key: process.env.pusher_key,
-    secret: process.env.pusher_secret,
-    cluster: process.env.pusher_cluster,
-  });
+const pusher = new Pusher({
+  appId: process.env.pusher_app_id,
+  key: process.env.pusher_key,
+  secret: process.env.pusher_secret,
+  cluster: process.env.pusher_cluster,
+});
 
-  pusher.trigger('channel-id-here', 'message', {
+module.exports = (req, res) => {
+  pusher.trigger("channel-id-here", "message", {
     message: "hello world",
   });
 
